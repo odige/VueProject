ca1d020de3b5cd024927ab42787c9fa00e4b831d
odige <1905448814@qq.com>
2022-11-14 20:21:33 +0800

    feat：新建vue

---
+++ newvue/src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="fixed-top head">
+    <div class="container">
+      <div class="row">
+        <img src="" alt="" class="col-2">
+        <ul class="nav col-8 ">
+          <li class="nav-item nav flex-column justify-content-center">
+            <a class="nav-link text-light" aria-current="page" href="#">商店</a>
+          </li>
+          <li class="nav-item nav flex-column justify-content-center">
+            <a class="nav-link text-light" href="#">社群</a>
+          </li>
+          <li class="nav-item nav flex-column justify-content-center">
+            <a class="nav-link text-light" href="#">关于</a>
+          </li>
+          <li class="nav-item nav flex-column justify-content-center">
+            <a class="nav-link text-light" href="#">客服中心</a>
+          </li>
+        </ul>
+        <ul class="nav col-1 offset-1">
+          <li class="nav-item nav flex-column justify-content-center">
+            <a class="nav-link text-light" href="#">账户</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <HelloWorld />
+  <div class="text-center foot">
+      <img src="" alt="">
+      <p>@梁友欢/林泽铨/李西东/邬昆佑</p>
+    </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+
+.head {
+  background-color: rgb(37, 42, 62, 0.8);
+  height: 80px;
+  margin: 15px auto;
+  width: 95%;
+  border-radius: 15px;
+  text-align: center;
+
+}
+.nav{
+  height: 80px;
+}
+body {
+  background-color: #172130;
+}
+.foot{
+  background-color:#171717 ;
+}
+.foot>p{
+  color: #8b8b8b;
+}
+</style>
--- newvue/src/components/HelloWorld.vue
+++ newvue/src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+   <div>
     <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
       <div class="carousel-indicators">
         <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
@@ -17,7 +17,7 @@
           <img src="../img/poster2.png" class="d-block w-100" alt="">
         </div>
         <div class="carousel-item">
-          <img src="" class="d-block w-100" alt="">
+          <img src="../img/poster1.png" class="d-block w-100" alt="">
         </div>
       </div>
       <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
@@ -34,19 +34,29 @@
   </div>
 </template>
 
-<script src="https://cdn.staticfile.org/twitter-bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
 <script>
 export default {
   name: 'HelloWorld',
-  data() {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+  props: {
+    msg: String
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
 </style>
