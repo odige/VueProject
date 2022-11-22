26f61cdd913ca931ff629fbadc0992c927982424
odige <1905448814@qq.com>
2022-11-22 14:28:55 +0800

    feat:增加评论页

---
+++ newvue/src/components/ComMent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Comment',
+    data(){
+        return{
+
+        }
+    }
+}
+
+</script>
+<style scoped>
+</style>
